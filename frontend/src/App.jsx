bfdea2c5e495113f841624a9f6057c94c8d728fa
solrxtin <onitahcelestine@gmail.com>
2024-10-11 20:57:17 +0100

    completed frontend auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import LoginPage from './pages/auth/login/LoginPage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import Sidebar from './components/common/Sidebar'
+import RightPanel from './components/common/RightPanel'
+import NotificationPage from './components/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
+import toast, { Toaster } from 'react-hot-toast'
+import { useQuery } from '@tanstack/react-query'
+import LoadingSpinner from './components/common/LoadingSpinner'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {data: authUser, isLoading} = useQuery({
+	queryKey: ["authUser"],
+	queryFn: async () => {
+		try {
+			const res = await fetch("/api/auth/authenticatedUser");
+			const data = await res.json();
 
+			if (data.error) return null
+
+			if (!res.ok) {
+				throw new Error(data.error || "Something went wrong")
+			}
+			return data;
+		} catch (error) {
+			toast(error.message)
+			throw new Error(error);
+		}
+	},
+  })
+
+  if (isLoading) {
+	return (
+		<div className="h-screen flex justify-center items-center">
+			<LoadingSpinner size='lg' />
+		</div>
+	)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+		{authUser && <Sidebar />}
+		<Routes>
+			<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+			<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+			<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+			<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
+			<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+		</Routes>
+		{authUser &&<RightPanel />}
+		<Toaster />
+    </div>
   )
 }
 
